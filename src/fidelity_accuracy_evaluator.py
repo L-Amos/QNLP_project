917a31441df41dfd43813daf132a3d563323d075
L-Amos <luke.amos77@outlook.com>
2024-10-23 14:49:56 +0100

    Updating imports to reflect changed

--- src/fidelity_accuracy_evaluator.py
+++ src/fidelity_accuracy_evaluator.py
@@ -1,7 +1,5 @@
-from state_fidelity import load_model, sentence_to_circuit, fidelity_test
+from state_fidelity import load_model, sentences_to_circuits, fidelity_test
 
 model=load_model(r"C:\Users\lukea\OneDrive\Documents\Uni Stuff\Master's\NLP Project\QNLP_project\testing\model.lt")
 
 fidelity, successes = fidelity_test("man prepares program .", "woman prepares program .", model)
-
-print(fidelity)
