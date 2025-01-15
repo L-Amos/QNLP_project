8349d6e001c169e0b405b0175dd5cd61f3e061d3
L-Amos <luke.amos.24@ucl.ac.uk>
2025-01-15 11:49:44 +0000

    Moving pair generation into its own function.

--- fidelity_model/train_data_generator.py
+++ fidelity_model/train_data_generator.py
@@ -24,52 +24,44 @@ SBERT_model = SentenceTransformer("all-MiniLM-L6-v2", similarity_fn_name=Similar
 all_sentences = ingest("data/all_sentences.txt")
 it_sentences = [sentence for sentence in all_sentences.keys() if all_sentences[sentence]=="0"]
 cooking_sentences = [sentence for sentence in all_sentences.keys() if all_sentences[sentence]=="1"]
-sentences_to_add = np.random.choice(list(all_sentences.keys()), size=80)  # For 160 training pairs
+sentences_to_add = np.random.choice(list(all_sentences.keys()), size=80, replace=False)  # For 160 training pairs
+
+def pair_generation(sentence_1, same_category):
+    ind = np.random.randint(0, len(it_sentences))
+    if same_category:
+        if sentence_1 in it_sentences:
+            sentence_2 = it_sentences[ind]
+        else:
+            sentence_2 = cooking_sentences[ind]
+    else:
+        if sentence_1 not in it_sentences:
+            sentence_2 = it_sentences[ind]
+        else:
+            sentence_2 = cooking_sentences[ind]
+    embeddings = SBERT_model.encode([sentence_1, sentence_2], normalize_embeddings=True)
+    similarity = return_similarity(embeddings)
+    return [sentence_1, sentence_2, similarity]
 
 ### TRAINING DATA ###
 # Both same
-for sentence_1 in sentences_to_add:
-    ind = np.random.randint(0, len(it_sentences))
-    if sentence_1 in it_sentences:
-        sentence_2 = it_sentences[ind]
-    else:
-        sentence_2 = cooking_sentences[ind]
-    embeddings = SBERT_model.encode([sentence_1, sentence_2], normalize_embeddings=True)
-    similarity = return_similarity(embeddings)
-    train_data.append([sentence_1, sentence_2, similarity])
+for sentence in sentences_to_add:
+    pair_data = pair_generation(sentence, same_category=True)
+    train_data.append(pair_data)
 # Both Different
-for sentence_1 in sentences_to_add:
-    ind = np.random.randint(0, len(it_sentences))
-    if sentence_1 not in it_sentences:
-        sentence_2 = it_sentences[ind]
-    else:
-        sentence_2 = cooking_sentences[ind]
-    embeddings = SBERT_model.encode([sentence_1, sentence_2], normalize_embeddings=True)
-    similarity = return_similarity(embeddings)
-    train_data.append([sentence_1, sentence_2, similarity])
+for sentence in sentences_to_add:
+    pair_data = pair_generation(sentence, same_category=False)
+    train_data.append(pair_data)
 
 ### VALIDATION DATA ###
 # Both same
 val_sentences = np.random.choice(sentences_to_add,size=20)  # For 40 validation pairs
-for sentence_1 in val_sentences:
-    ind = np.random.randint(0, len(it_sentences))
-    if sentence_1 in it_sentences:
-        sentence_2 = it_sentences[ind]
-    else:
-        sentence_2 = cooking_sentences[ind]
-    embeddings = SBERT_model.encode([sentence_1, sentence_2], normalize_embeddings=True)
-    similarity = return_similarity(embeddings)
-    val_data.append([sentence_1, sentence_2, similarity])
+for sentence in val_sentences:
+    pair_data = pair_generation(sentence, same_category=True)
+    val_data.append(pair_data)
 # Both Different
-for sentence_1 in val_sentences:
-    ind = np.random.randint(0, len(it_sentences))
-    if sentence_1 not in it_sentences:
-        sentence_2 = it_sentences[ind]
-    else:
-        sentence_2 = cooking_sentences[ind]
-    embeddings = SBERT_model.encode([sentence_1, sentence_2], normalize_embeddings=True)
-    similarity = return_similarity(embeddings)
-    val_data.append([sentence_1, sentence_2, similarity])
+for sentence in val_sentences:
+    pair_data = pair_generation(sentence, same_category=False)
+    val_data.append(pair_data)
 
 # Save to File
 np.savetxt("data/train_data.csv",train_data, delimiter=",",  fmt="%s", header="sentence_1,sentence_2,label", comments="")
