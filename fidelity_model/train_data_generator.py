f0233b93b1f499449171217134b2abfbb49e5d48
L-Amos <luke.amos.24@ucl.ac.uk>
2025-01-15 10:57:00 +0000

    Doubling amount of training and validation data.

--- fidelity_model/train_data_generator.py
+++ fidelity_model/train_data_generator.py
@@ -24,7 +24,7 @@ SBERT_model = SentenceTransformer("all-MiniLM-L6-v2", similarity_fn_name=Similar
 all_sentences = ingest("data/all_sentences.txt")
 it_sentences = [sentence for sentence in all_sentences.keys() if all_sentences[sentence]=="0"]
 cooking_sentences = [sentence for sentence in all_sentences.keys() if all_sentences[sentence]=="1"]
-sentences_to_add = np.random.choice(list(all_sentences.keys()), size=40)
+sentences_to_add = np.random.choice(list(all_sentences.keys()), size=80)  # For 160 training pairs
 
 ### TRAINING DATA ###
 # Both same
@@ -50,7 +50,7 @@ for sentence_1 in sentences_to_add:
 
 ### VALIDATION DATA ###
 # Both same
-val_sentences = np.random.choice(sentences_to_add,size=10)  # For 20 validation pairs
+val_sentences = np.random.choice(sentences_to_add,size=20)  # For 40 validation pairs
 for sentence_1 in val_sentences:
     ind = np.random.randint(0, len(it_sentences))
     if sentence_1 in it_sentences:
