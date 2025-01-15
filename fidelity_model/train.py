d6336b1d0d8794f751074578c216a5f3066850cf
L-Amos <luke.amos.24@ucl.ac.uk>
2025-01-15 12:19:43 +0000

    Changing to systematically train for different hyperparameters.

--- fidelity_model/train.py
+++ fidelity_model/train.py
@@ -8,10 +8,11 @@ from fidelity_model import FidelityModel, fidelity_pqc_gen
 EPOCHS=240
 BATCH_SIZE=30
 SEED=2
-A=1
 C=0.06
 
-# Parse Train Data
+def loss(predictions, labels):
+    return np.mean((predictions-labels)**2)
+
 print("Reading Train Data...")
 with open("data/train_data.csv", "r", encoding="utf8") as f:
     csvfile = csv.DictReader(f)
@@ -33,19 +34,6 @@ with open("data/val_data.csv", "r", encoding="utf8") as f:
 print("Generating Val PQCs...")
 val_circuits = [fidelity_pqc_gen(sentence_1, sentence_2) for sentence_1, sentence_2 in val_pairs]
 
-def loss(predictions, labels):
-    return np.mean((predictions-labels)**2)
-
-model = FidelityModel.from_diagrams(train_circuits+val_circuits, use_jit=True)
-trainer = QuantumTrainer(
-    model,
-    loss_function=loss,
-    epochs=EPOCHS,
-    optimizer=SPSAOptimizer,
-    optim_hyperparams={'a': A, 'c': C, 'A':0.01*EPOCHS},
-    seed=SEED
-)
-
 train_dataset = Dataset(
         train_circuits,
         train_labels,
@@ -55,8 +43,21 @@ val_dataset = Dataset(
         val_labels,
         batch_size=BATCH_SIZE)
 
-print("===TRAINING===")
-trainer.fit(train_dataset, val_dataset, log_interval=1)
+a_vals = np.arange(0.05, 1.01, 0.05)
+for a in a_vals:
+    # Parse Train Data
+    model = FidelityModel.from_diagrams(train_circuits+val_circuits, use_jit=True)
+    trainer = QuantumTrainer(
+        model,
+        loss_function=loss,
+        epochs=EPOCHS,
+        optimizer=SPSAOptimizer,
+        optim_hyperparams={'a': a, 'c': C, 'A':0.01*EPOCHS},
+        seed=SEED
+    )
 
-np.savetxt("latest_train_costs.csv", trainer.train_epoch_costs, delimiter=',')
-np.savetxt("latest_val_costs.csv", trainer.val_costs, delimiter=',')
+    print(f"===TRAINING: a-{a}===")
+    trainer.fit(train_dataset, val_dataset, log_interval=12)
+
+    np.savetxt(f"data/C=0.06/a-{a}_train_costs.csv", trainer.train_epoch_costs, delimiter=',')
+    np.savetxt(f"data/C=0.06/a-{a}_val_costs.csv", trainer.val_costs, delimiter=',')
