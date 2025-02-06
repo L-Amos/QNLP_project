01de71ddfa8490d2c43526a48a27cede3bd19e95
L-Amos <luke.amos.24@ucl.ac.uk>
2025-02-06 10:48:04 +0000

    Refactoring to have more test + train pairs + therefore removing need to manually pick balance for same/different categories (law of averages will ensure balance).

--- fidelity_model/train_data_generator.py
+++ fidelity_model/train_data_generator.py
@@ -11,6 +11,7 @@ def ingest(file_path):
     # Retrieve train sentences + parse
     return data
 
+# Get SBERT Similarity
 def return_similarity(embeddings):
     SBERT_similarity = np.abs(float(SBERT_model.similarity(embeddings[0], embeddings[1])))  # |<phi|psi>|
     if SBERT_similarity > 1:  # Sometimes > 1 due to floating point errors
@@ -21,56 +22,29 @@ def return_similarity(embeddings):
 train_data = []
 val_data = []
 SBERT_model = SentenceTransformer("all-MiniLM-L6-v2", similarity_fn_name=SimilarityFunction.DOT_PRODUCT)
-all_sentences = ingest("data/all_sentences.txt")
+all_sentences = ingest("data/train_sentences.txt")
 it_sentences = [sentence for sentence in all_sentences.keys() if all_sentences[sentence]=="0"]
 cooking_sentences = [sentence for sentence in all_sentences.keys() if all_sentences[sentence]=="1"]
-sentences_to_add = np.random.choice(list(all_sentences.keys()), size=80, replace=False)  # For 160 training pairs
 
-def pair_generation(sentence_1, same_category):
-    ind = np.random.randint(0, len(it_sentences))
-    if same_category:
-        if sentence_1 in it_sentences:
-            sentence_2 = it_sentences[ind]
-        else:
-            sentence_2 = cooking_sentences[ind]
-    else:
-        if sentence_1 not in it_sentences:
-            sentence_2 = it_sentences[ind]
-        else:
-            sentence_2 = cooking_sentences[ind]
-    embeddings = SBERT_model.encode([sentence_1, sentence_2], normalize_embeddings=True)
-    similarity = return_similarity(embeddings)
-    return [sentence_1, sentence_2, similarity]
+TEST_PAIRS = 500
+VAL_PAIRS = 100
 
 ### TRAINING DATA ###
-# Both same
-for sentence in sentences_to_add:
-    pair_data = pair_generation(sentence, same_category=True)
-    while pair_data in train_data:
-        pair_data = pair_generation(sentence, same_category=True)
-    train_data.append(pair_data)
-# Both Different
-for sentence in sentences_to_add:
-    pair_data = pair_generation(sentence, same_category=False)
-    while pair_data in train_data:
-        pair_data = pair_generation(sentence, same_category=True)
+for i in range(TEST_PAIRS):
+    test_sentences = np.random.choice(list(all_sentences.keys()), size=2, replace=False)
+    test_embeddings = SBERT_model.encode(test_sentences, normalize_embeddings=True)
+    similarity = return_similarity(test_embeddings)
+    pair_data = [test_sentences[0], test_sentences[1], similarity]
     train_data.append(pair_data)
 
 ### VALIDATION DATA ###
-# Both same
-val_sentences = np.random.choice(sentences_to_add,size=20)  # For 40 validation pairs
-for sentence in val_sentences:
-    pair_data = pair_generation(sentence, same_category=True)
-    while pair_data in train_data or pair_data in val_data:
-        pair_data = pair_generation(sentence, same_category=True)
-    val_data.append(pair_data)
-# Both Different
-for sentence in val_sentences:
-    pair_data = pair_generation(sentence, same_category=False)
-    while pair_data in train_data or pair_data in val_data:
-        pair_data = pair_generation(sentence, same_category=True)
-    val_data.append(pair_data)
+for i in range(VAL_PAIRS):
+    val_sentences = np.random.choice(list(all_sentences.keys()), size=2, replace=False)
+    val_embeddings = SBERT_model.encode(test_sentences, normalize_embeddings=True)
+    similarity = return_similarity(val_embeddings)
+    pair_data = [val_sentences[0], val_sentences[1], similarity]
+    train_data.append(pair_data)
 
 # Save to File
-np.savetxt("data/train_data.csv",train_data, delimiter=",",  fmt="%s", header="sentence_1,sentence_2,label", comments="")
-np.savetxt("data/val_data.csv",val_data, delimiter=",",  fmt="%s", header="sentence_1,sentence_2,label", comments="")
+np.savetxt("data/train_data_alt.csv",train_data, delimiter=",",  fmt="%s", header="sentence_1,sentence_2,label", comments="")
+np.savetxt("data/val_data_alt.csv",val_data, delimiter=",",  fmt="%s", header="sentence_1,sentence_2,label", comments="")
