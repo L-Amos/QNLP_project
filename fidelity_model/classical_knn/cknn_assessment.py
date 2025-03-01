f252be3513c0c5804e2b0709b8780b1b9869004f
L-Amos <luke.amos.24@ucl.ac.uk>
2025-03-01 20:05:52 +0000

    Using the model to calculate fidelities rather than getting the states + calculating them with numpy.

--- fidelity_model/classical_knn/cknn_assessment.py
+++ fidelity_model/classical_knn/cknn_assessment.py
@@ -1,24 +1,11 @@
 from tqdm import tqdm
 from os import sys
 sys.path.append("../")
-from fidelity_model import FidelityModel
+from fidelity_model import FidelityModel, fidelity_pqc_gen
 from utils import ingest, sentence_pqc_gen, get_states
 import numpy as np
 from collections import Counter
 
-
-def get_sentence_states(sentences, model, language_model):
-    # Create PQCs
-    progress_bar = tqdm(sentences, bar_format="{desc}{percentage:3.0f}%|{bar:25}{r_bar}")
-    progress_bar.set_description("Generating Sentence Circuits")
-    circuits = [sentence_pqc_gen(sentence, language_model) for sentence in progress_bar]
-    # Substitute Model Weights
-    print("Subbing Model Weights...", end="")
-    diags = model._fast_subs(circuits, model.weights)
-    print("Done")
-    # Run Train Sentences Through Model
-    states = get_states(diags, description="Generating States")
-    return states
 
 def main():
     # Load Model
@@ -34,23 +21,20 @@ def main():
     train_sentences = train_data.keys()
     test_data = ingest("../model_training/data/test_sentences.txt")
     test_sentences = test_data.keys()
-    print("Done")
-    # Get train and test sentece states
-    print("# TRAIN SENTENCES #")
-    train_states = get_sentence_states(train_sentences, model, LANGUAGE_MODEL)
-    print("# TEST SENTENCES #")
-    test_states = get_sentence_states(test_sentences, model, LANGUAGE_MODEL)    
+    print("Done")  
     # Get k nearest neighbours
     K=5
     correct = 0
     print("# MEASURING ACCURACY # ")
     progress_bar = tqdm(test_sentences, bar_format="{desc}{percentage:3.0f}%|{bar:25}{r_bar}")
     progress_bar.set_description("Classifying With KNN")
-    for test_sentence, test_state in zip(progress_bar, test_states):
-        fidelities = []
-        for train_sentence, train_state in zip(train_sentences, train_states):
-            fidelities.append([train_data[train_sentence], np.abs(test_state@train_state)**2])  # Find fidelity between test and train state
-        fidelities = np.array(fidelities)  # Needed for argsort in next line
+
+    for test_sentence in progress_bar:
+        circuits = []
+        for train_sentence in train_sentences:
+            circuits.append(fidelity_pqc_gen(test_sentence, train_sentence, LANGUAGE_MODEL))
+        model_results = model(circuits)  # Needed for argsort in next line
+        fidelities = np.array([[label, fidelity] for label, fidelity in zip(list(train_data.values()), model_results)])
         sorted_fidelities = fidelities[fidelities[:, 1].argsort()]
         highest_fidelities = sorted_fidelities[-K:]
         highest_fidelity_labels = highest_fidelities[:,0]
