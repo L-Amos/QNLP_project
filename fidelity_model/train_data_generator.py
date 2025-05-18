b26d1e5d515502cc24537c43aad4571bbbaaff77
L-Amos <luke.amos.24@ucl.ac.uk>
2025-05-18 09:43:03 +0100

    Incorporating binary labels to ensure same pairs for both types of label.

--- fidelity_model/train_data_generator.py
+++ fidelity_model/train_data_generator.py
@@ -21,8 +21,10 @@ def return_similarity(embeddings):
 # Read Sentences
 train_data = []
 val_data = []
+train_data_binary = []
+val_data_binary = []
 SBERT_model = SentenceTransformer("all-MiniLM-L6-v2", similarity_fn_name=SimilarityFunction.DOT_PRODUCT)
-all_sentences = ingest("data/train_sentences.txt")
+all_sentences = ingest("model_training/data/train_sentences.txt")
 it_sentences = [sentence for sentence in all_sentences.keys() if all_sentences[sentence]=="0"]
 cooking_sentences = [sentence for sentence in all_sentences.keys() if all_sentences[sentence]=="1"]
 
@@ -31,20 +33,39 @@ VAL_PAIRS = 100
 
 ### TRAINING DATA ###
 for i in range(TEST_PAIRS):
-    test_sentences = np.random.choice(list(all_sentences.keys()), size=2, replace=False)
-    test_embeddings = SBERT_model.encode(test_sentences, normalize_embeddings=True)
-    similarity = return_similarity(test_embeddings)
-    pair_data = [test_sentences[0], test_sentences[1], similarity]
+    train_sentences = np.random.choice(list(all_sentences.keys()), size=2, replace=False)
+    train_embeddings = SBERT_model.encode(train_sentences, normalize_embeddings=True)
+    # SBERT Similarity
+    similarity = return_similarity(train_embeddings)
+    pair_data = [train_sentences[0], train_sentences[1], similarity]
     train_data.append(pair_data)
+    # Binary Similarity
+    if np.all(np.isin(train_sentences, it_sentences)) or np.all(np.isin(train_sentences, cooking_sentences)):  # If both same category
+        similarity = 1.0
+    else:
+        similarity = 0.0
+    pair_data = [train_sentences[0], train_sentences[1], similarity]
+    train_data_binary.append(pair_data)
 
 ### VALIDATION DATA ###
 for i in range(VAL_PAIRS):
     val_sentences = np.random.choice(list(all_sentences.keys()), size=2, replace=False)
-    val_embeddings = SBERT_model.encode(test_sentences, normalize_embeddings=True)
+    val_embeddings = SBERT_model.encode(train_sentences, normalize_embeddings=True)
+    # SBERT Similarity
     similarity = return_similarity(val_embeddings)
     pair_data = [val_sentences[0], val_sentences[1], similarity]
-    train_data.append(pair_data)
+    val_data.append(pair_data)
+    # Binary Similarity
+    if np.all(np.isin(val_sentences, it_sentences)) or np.all(np.isin(val_sentences, cooking_sentences)):  # If both same category
+        similarity = 1.0
+    else:
+        similarity = 0.0
+    pair_data = [val_sentences[0], val_sentences[1], similarity]
+    val_data_binary.append(pair_data)
+
 
 # Save to File
-np.savetxt("data/train_data_alt.csv",train_data, delimiter=",",  fmt="%s", header="sentence_1,sentence_2,label", comments="")
-np.savetxt("data/val_data_alt.csv",val_data, delimiter=",",  fmt="%s", header="sentence_1,sentence_2,label", comments="")
+np.savetxt("model_training/data/train_data_similarity.csv",train_data, delimiter=",",  fmt="%s", header="sentence_1,sentence_2,label", comments="")
+np.savetxt("model_training/data/val_data_similarity.csv",val_data, delimiter=",",  fmt="%s", header="sentence_1,sentence_2,label", comments="")
+np.savetxt("model_training/data/train_data_binary.csv",train_data_binary, delimiter=",",  fmt="%s", header="sentence_1,sentence_2,label", comments="")
+np.savetxt("model_training/data/val_data_binary.csv",val_data_binary, delimiter=",",  fmt="%s", header="sentence_1,sentence_2,label", comments="")
